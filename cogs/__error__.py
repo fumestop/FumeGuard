9be57183e795423aafeb7fca50cfea1beeb4ec4d
Sayan Bhattacharyya <sn1f3rt@outlook.com>
2023-12-27 11:38:30 +0530

    Reformatting

--- cogs/__error__.py
+++ cogs/__error__.py
@@ -48,9 +48,7 @@ class Error(commands.Cog):
                 embed.description = ""
 
                 embed.add_field(name="Command", value=f"`{command.name}`", inline=False)
-                embed.add_field(
-                    name="Exception", value=f"```css\n{str(error)}```"
-                )
+                embed.add_field(name="Exception", value=f"```css\n{str(error)}```")
 
                 channel = self.bot.get_channel(self.log_channel_id)
 
